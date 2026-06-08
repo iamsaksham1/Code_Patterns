55e869cc377e8e1396a58e55ecadd0d1c4be0c9a
”Saksham <iamsakshamrathore@gmail.com>
2026-06-08 23:21:03 +0530

    Initial commit

---
+++ 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x=5;
+    for (int i=0; i<x; i++){
+        for(int j=0; j<i; j++){
+            cout<<"*";
+        }
+        cout <<endl;
+    }
+    return 0;
+}
