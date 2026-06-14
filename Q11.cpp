7632490b97d1aef7396e2d1140a60cfb11d6c8a1
Saksham Rathore <iamsakshamrathore@gmail.com>
2026-06-14 18:30:37 +0530

    star pattern both up and down

---
+++ Q11.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    for(int i=0; i<n; i++){
+        for(int j=1; j<=i+1; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=n-1; i>=1; i--){
+        for(int j =i; j>=1; j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+return 0;
+}
